3f92970a35265193db4397427163940eba370d45
syzer3@gmail.com <syzer3@gmail.com>
2018-02-13 12:13:46 +0100

    frozen lake

--- dev/my_frozen_lake.py
+++ dev/my_frozen_lake.py
@@ -1,8 +1,35 @@
+# todo https://gym.openai.com/evaluations/eval_lEi8I8v2QLqEgzBxcvRIaA/
 import gym
+
 env = gym.make('FrozenLake-v0')
-env.reset()
+env.reset() # => observation
 env.render()
-env.reset()
+
+
+def random_action():
+    return env.action_space.sample()
+
+
+env.step(random_action())
+# print(env.action_space)
+# print(env.observation_space)
+
+env.render()
+# return observations
+# (1, 0.0, False, {'prob': 0.3333333333333333})
+
+
+observation, reward, done, info = env.step(random_action())
+print(observation)
+# observation in the frozen lake is is the how far we got
+
+env.render()
+action = 1 if observation > 0 else 2
+env.step(action)
+action = 1 if observation > 0 else 2
+env.step(action)
+action = 1 if observation > 0 else 2
+env.step(action)
 env.render()
 
 # SFFF       (S: starting point, safe)
